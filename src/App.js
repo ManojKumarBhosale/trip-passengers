9ddcb0837e4afa44edb0422a9ca09eb9d175ec6d
Manoj <manojbhosale36@gmail.com>
2019-12-21 19:31:07 +0530

    Adding basic react app structure

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        Passenger details
+      </header>
+      <main>
+        Details
+      </main>
+      <footer>
+        Footer section
+      </footer>
+    </div>
+  );
+}
+
+export default App;
