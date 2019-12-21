4234d59ec130dfbbf27767879466fec7138999ab
Manoj <manojbhosale36@gmail.com>
2019-12-21 18:32:05 +0530

    Updates

---
+++ precache-manifest.7419a00c37f4d1e0b2d19c066e37fa7e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6ec798e347ca419a20b40c05d68d3e4f",
+    "url": "/ManojKumarBhosale/trip-passengers/index.html"
+  },
+  {
+    "revision": "18379a3b79b744289012",
+    "url": "/ManojKumarBhosale/trip-passengers/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "1d285a3937a25e7dab7f",
+    "url": "/ManojKumarBhosale/trip-passengers/static/js/2.bc9b0647.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/ManojKumarBhosale/trip-passengers/static/js/2.bc9b0647.chunk.js.LICENSE"
+  },
+  {
+    "revision": "18379a3b79b744289012",
+    "url": "/ManojKumarBhosale/trip-passengers/static/js/main.d51c4980.chunk.js"
+  },
+  {
+    "revision": "a8a497b82f0c05614757",
+    "url": "/ManojKumarBhosale/trip-passengers/static/js/runtime-main.f1529dfd.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ManojKumarBhosale/trip-passengers/static/media/logo.5d5d9eef.svg"
+  }
+]);
